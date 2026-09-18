b2ddd0d00b70254ed7bb0880bdb99e137f8d8324
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2026-09-18 14:27:41 +0200

    Npc Dialogue interaction
    basic script and test scene

---
+++ Assets/AKW_Folder/AKW_scripts/NPC_Dialogue_test.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class NPC_Dialogue_test : MonoBehaviour
+{
+
+    [SerializeField] WorldInteractable NpcCollider;
+    public GameObject DialogueBox;
+
+    public void OnEnable()
+    {
+        NpcCollider.OnInteract += OnNpcInteraction;
+    }
+
+    public async void OnNpcInteraction()
+    {
+        Debug.Log("NPC Interaction");
+        await Awaitable.WaitForSecondsAsync(0.1f);
+        if (DialogueBox.activeSelf == false)
+        {
+            DialogueBox.SetActive(true);
+            Debug.Log("NPC Interaction is going on");            
+        }
+        else
+        {
+            DialogueBox.SetActive(false);
+            Debug.Log("NPC Interaction is going on");
+        }
+    }   
+}
