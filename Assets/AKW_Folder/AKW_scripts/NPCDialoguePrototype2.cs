de93a3b43151f1793ba08b48c937e2f57e4b71d1
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2026-09-19 11:16:54 +0200

    NPC Dialogue prototype script V.2
    A script that will allow multiple dialogue boxes in to be activated in sequence on interaction

---
+++ Assets/AKW_Folder/AKW_scripts/NPCDialoguePrototype2.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NPCDialoguePrototype2 : MonoBehaviour
+{
+    [SerializeField] WorldInteractable NpcCollider;
+
+    public List<GameObject> DialogueBoxes;
+
+    private int currentDialogueBox = -1;
+    //private int oldDialogueBox = -2;
+
+    public UnityEvent onDialogueFinished = new UnityEvent();
+
+    public void OnEnable()
+    {
+        NpcCollider.OnInteract += OnNpcInteraction;
+    }
+
+    public void OnDisable()
+    {
+        NpcCollider.OnInteract -= OnNpcInteraction;
+    }
+    public void OnNpcInteraction()
+    {
+        Debug.Log("NPC Interaction 2, electric boogaloo");
+        currentDialogueBox += 1;
+
+        DialogueBoxes[currentDialogueBox - 1].gameObject.SetActive(false);
+        DialogueBoxes[currentDialogueBox].gameObject.SetActive(true);
+
+        if (currentDialogueBox > DialogueBoxes.Count - 1)
+        {
+            DialogueBoxes[currentDialogueBox].gameObject.SetActive(false);
+            currentDialogueBox = -1;
+        }
+    }
+}
